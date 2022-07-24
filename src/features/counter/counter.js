647a26614ef324051a3eca9ee880927e8c4b4a8f
GairikSharma <88035292+GairikSharma@users.noreply.github.com>
2022-07-25 00:52:57 +0530

    Update counter.js

--- src/features/counter/counter.js
+++ src/features/counter/counter.js
@@ -1,19 +1,18 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 import React, { useState } from "react";
+import { decrement, increment } from "./counterSlice";
 
 function Counter() {
     const count = useSelector((state) => state.counter.count)
+    const dispatch = useDispatch()
+
     return (
         <>
             <h1>Hello React Redux</h1>
-            <button onClick={() => {
-                
-            }}>-</button>
+            <button onClick={() => dispatch(decrement())}>-</button>
             <span>Count : {count}</span>
-            <button onClick={() => {
-                
-            }}>+</button>
+            <button onClick={() => dispatch(increment())}>+</button>
         </>
     )
 }
